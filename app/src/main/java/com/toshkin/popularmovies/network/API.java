2f7ea0cedc3b2407c51d6fdde4019cb107f0be2d
Lazar Toshkin <toshkinl@gmail.com>
2015-11-16 22:46:16 +0200

    Added additional API calls

--- app/src/main/java/com/toshkin/popularmovies/network/API.java
+++ app/src/main/java/com/toshkin/popularmovies/network/API.java
@@ -2,6 +2,7 @@ package com.toshkin.popularmovies.network;
 
 import retrofit.Callback;
 import retrofit.http.GET;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 /**
@@ -11,5 +12,11 @@ public interface API {
 
     @GET("/discover/movie")
     void getMovies(@Query("sort_by") String sortType, Callback<MoviesResponse> callback);
+
+    @GET("/movie/{id}/videos")
+    void getTrailer(@Path("id") int movieId, Callback<TrailerResponse> callback);
+
+    @GET("/movie/{id}/reviews")
+    void getReviews(@Query("id") int movieId, Callback<ReviewsResponse> callback);
 
 }
